b65cb7a34b195d3f2abd8d5e5aeecad882edcc58
Érico Ribeiro <oliv.ericorib@gmail.com>
2025-02-03 18:27:51 -0300

    feat: setup swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Auth System')
+    .setDescription('An authentication/authorization API')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.SERVER_PORT ?? 3000);
 }
 bootstrap();
